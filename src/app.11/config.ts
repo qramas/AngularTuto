171a8f9acf4c17155b47b6d6185c8e7843f90429
RAMAS <quentin.ramas@capgemini.com>
2017-07-04 14:25:05 +0200

    Suite formation voir autre branch pour le début

--- src/app.11/config.ts
+++
@@ -1,4 +0,0 @@
-export class Config {
-    public static get APP_TITLE(): string { return "Application livreur" };
-    public static get APP_VERSION(): string { return "1.0" };
-}
--- src/app.8/config.ts
+++
@@ -1,5 +0,0 @@
-//        8.1.1  export class config avec 2 fonctions static get
-export class Config {
-    public static get APP_TITLE(): string { return "Application livreur" };
-    public static get APP_VERSION(): string { return "1.0" };
-}

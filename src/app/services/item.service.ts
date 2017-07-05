92cd94c49de460fb5e0763def46569b1ff5bbd1c
RAMAS <quentin.ramas@capgemini.com>
2017-07-05 14:01:14 +0200

    itemservice

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -4,19 +4,14 @@ import { Item } from "app/models/item";
 @Injectable()
 export class ItemService {
 
-  public newItem: Item;
   public collection: Item[];
 
   constructor() {
     this.collection = [new Item({ reference: '1234', name: 'quentin', state: 0 })];
-    this.resetNewItem();
   }
 
   public add(item: Item): void {
     this.collection.unshift(item);
-    this.resetNewItem();
   }
-  public resetNewItem(): void {
-    this.newItem = new Item({ reference: '', name: '', state: 0 });
-  }
+ 
 }
